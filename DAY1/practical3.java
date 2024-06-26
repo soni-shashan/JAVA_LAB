5583f63c5fc6e1281df98a0997857a097b55e956
soni-shashan <lumbhanishashan1510@gmail.com>
2024-06-26 15:31:37 +0530

    add

--- DAY1/practical3.java
+++ DAY1/practical3.java
@@ -3,7 +3,7 @@ public class practical3
 {
 	public static void main(String[] args){
 		double d=1234.5678;
-		int i=(int)d;
+		long i=(long)d;
 		short s;
 		s=(short)((d-i)*10000);
 		System.out.println("Integer Part: "+i+"   Fraction Part: "+s);
